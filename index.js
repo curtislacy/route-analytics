85088f9efa30a65fe81944e0986039a0c00098f6
curtis.lacy@ldengine.com <cmlacy@crispin.home>
2012-11-26 08:41:16 -0500

    Initial revision of the logging code.

---
+++ index.js
@@ -0,0 +1,38 @@
+function Logger( config ) {
+	this.winston = new ( require( 'winston' ).Logger)( config );
+}
+Logger.prototype.logRequest = function( req ) {
+	var url = req.url;
+	if( req.user )
+		var user = req.user.identifier;
+	else
+		var user = 'none';
+
+	this.winston.info( ( Date.now() ) + ', ROUTE-ANALYTICS, request, ' + url + ', ' + user );
+}
+Logger.prototype.logResponse = function( req, res ) {
+	var url = req.url;
+	if( req.user )
+		var user = req.user.identifier;
+	else
+		var user = 'none';
+
+	this.winston.info( ( Date.now() ) + ', ROUTE-ANALYTICS, response, ' + url + ', ' + user );
+}
+
+module.exports = exports = function( config ) {
+	var config = config;
+
+	var log = new Logger( config );
+
+	return function( req, res, next ) {
+		log.logRequest( req );
+
+		var endFunction = res.end;
+		res.end = function( data, encoding ) {
+			log.logResponse( req, res );
+			endFunction( data, encoding );
+		}
+		next();
+	};
+};
